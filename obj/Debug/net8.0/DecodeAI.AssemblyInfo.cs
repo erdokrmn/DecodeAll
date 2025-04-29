263b62659a111c885c00b7f39f1baff73a69471d
Erdinç Karaman <erdokrmn@hotmail.com>
2025-04-29 16:31:40 +0300

    erdokrmn

--- obj/Debug/net8.0/DecodeAI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DecodeAI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DecodeAI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cbd189aacfe619bd08cf31aadcf51afcb44a45c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DecodeAI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DecodeAI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
